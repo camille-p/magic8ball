0cf4c48bbcf8f9cc00243aaffbb5ae0a4b5c7bb1
Camille <puyocamille@gmail.com>
2020-12-05 18:58:37 +0000

    first commit

---
+++ magicEightball-fr.js
@@ -0,0 +1,64 @@
+let randomNumber = Math.floor(Math.random() * 20);
+let eightBall = ' '
+switch (randomNumber) {
+    case 0:
+    eightBall = "D'après moi oui";
+    break;
+    case 1:
+    eightBall = "C'est certain";
+    break;
+    case 2:
+    eightBall = "Oui absolument";
+    break;
+    case 3:
+    eightBall = "Tu peux compter dessus";
+    break;
+    case 4:
+    eightBall = "Sans aucun doute";
+    break;
+    case 5:
+    eightBall = "Très probable";
+    break;
+    case 6:
+    eightBall = "Oui";
+    break;
+    case 7:
+    eightBall = "C'est bien parti";
+    break;
+    case 8:
+    eightBall = "Essaye plus tard";
+    break;
+    case 9:
+    eightBall = "Essaye encore";
+    break;
+    case 10:
+    eightBall = "Impossible";
+    break;
+    case 11:
+    eightBall = "Pas d'avis";
+    break;
+    case 12:
+    eightBall = "C'est ton destin";
+    break;
+    case 13:
+    eightBall = "Le sort en est jeté";
+    break;
+    case 14:
+    eightBall = "Une chance sur deux";
+    break;
+    case 15:
+    eightBall = "Repose ta question";
+    break;
+    case 16:
+    eightBall = "C'est non";
+    break;
+    case 17:
+    eightBall = "Peu probable";
+    break;
+    case 18:
+    eightBall = "Faut pas rêver";
+    break;
+    case 19:
+    eightBall = "N'y compte pas";
+    break;
+}
